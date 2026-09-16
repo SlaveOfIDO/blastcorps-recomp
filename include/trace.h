7772655a16ef93f3cdf0d852b82bb86a0dd88f8d
SlaveOfIDO <slaveofido@nyon.de>
2026-09-16 15:22:07 +0200

    more trace info

--- include/trace.h
+++ include/trace.h
@@ -5,11 +5,19 @@
 
 #define TRACE_ENTRY() \
     static int was_called = 0; \
+    static int was_returned = 0; \
     if (was_called == 0) { \
-        fprintf(stderr, "%s\n", __func__); \
+        fprintf(stderr, "<%s>\n", __func__); \
         was_called = 1; \
     }
-#define TRACE_RETURN()// fprintf(stderr, "TRACE_RETURN(%s)\n", __func__);
+#define TRACE_RETURN() \
+    if (was_returned == 0) {                   \
+        fprintf(stderr, "</%s>\n", __func__); \
+        was_returned = 1;                      \
+    }
 #define TRACE_MEMWRITE() ((void*))
 #define TRACE_MEMREAD() ((void*))
+
+
+
 #endif // __SSB_TRACE_H__
