2f8a9e829b22752e0ced7d9e72e14519bf3059b8
SlaveOfIDO <slaveofido@nyon.de>
2026-09-16 15:14:59 +0200

    update sched.h

--- include/blast_sched.h
+++ include/blast_sched.h
@@ -33,49 +33,20 @@
 
 #include <ultra64.h>
 
-#define OS_SC_STACKSIZE      0x2000
+#define OS_SC_STACKSIZE 0x2000
 
-#define OS_SC_RETRACE_MSG       1
-#define OS_SC_DONE_MSG          2
-#define OS_SC_RDP_DONE_MSG      3
-#define OS_SC_PRE_NMI_MSG       4
-#define OS_SC_LAST_MSG          4	/* this should have highest number */
-#define OS_SC_MAX_MESGS         8
+#define OS_SC_RETRACE_MSG 1
+#define OS_SC_DONE_MSG 2
+#define OS_SC_RDP_DONE_MSG 3
+#define OS_SC_PRE_NMI_MSG 4
+#define OS_SC_LAST_MSG 4 /* this should have highest number */
+#define OS_SC_MAX_MESGS 16
 
 typedef struct {
     short type;
-    char  misc[30];
+    char misc[30];
 } OSScMsg;
 
-typedef struct OSScTask_s {
-    struct OSScTask_s   *next;          /* note: this must be first */
-    u32                 state;
-    u32			flags;
-    void		*framebuffer;	/* used by graphics tasks */
-
-    OSTask              list;
-    OSMesgQueue         *msgQ;
-    OSMesg              msg;
-    s32 unk58; // some flags
-    s32 pad2; // TODO: this can be wrong
-#ifndef _FINALROM                       /* all #ifdef items should    */
-    OSTime              startTime;      /* remain at the end!!, or    */
-    OSTime              totalTime;      /* possible conflict if       */
-#endif                                  /* FINALROM library used with */
-} OSScTask;                             /* non FINALROM code          */
-
-/*
- * OSScTask flags:
- */
-#define OS_SC_NEEDS_RDP	        0x0001	/* uses the RDP */
-#define OS_SC_NEEDS_RSP	        0x0002  /* uses the RSP */
-#define OS_SC_DRAM_DLIST        0x0004  /* SP & DP communicate through DRAM */
-#define OS_SC_PARALLEL_TASK     0x0010	/* must be first gfx task on list */
-#define OS_SC_LAST_TASK	        0x0020	/* last task in queue for frame */
-#define OS_SC_SWAPBUFFER        0x0040	/* swapbuffers when gfx task done */
-
-#define OS_SC_RCP_MASK		0x0003	/* mask for needs bits */
-#define OS_SC_TYPE_MASK		0x0007	/* complete type mask */
 /*
  * OSScClient:
  *
@@ -84,42 +55,66 @@ typedef struct OSScTask_s {
  *
  */
 typedef struct SCClient_s {
-    struct SCClient_s   *next;  /* next client in the list      */
-    OSMesgQueue         *msgQ;  /* where to send the frame msg  */
+    struct SCClient_s* next; /* next client in the list      */
+    OSMesgQueue* msgQ;       /* where to send the frame msg  */
     s32 unk8;
     s32 unkC;
 } OSScClient;
 
+typedef struct OSScTask_s {
+    struct OSScTask_s* next; /* note: this must be first */
+    u32 state;
+    u32 flags;
+    void* framebuffer; /* used by graphics tasks */
+
+    OSTask list;
+    OSScClient* client;
+    OSMesgQueue* msgQ;
+    OSMesg msg;       // some flags
+    s32 pad2;         // TODO: this can be wrong
+#ifndef _FINALROM     /* all #ifdef items should    */
+    OSTime startTime; /* remain at the end!!, or    */
+    OSTime totalTime; /* possible conflict if       */
+#endif                /* FINALROM library used with */
+} OSScTask;           /* non FINALROM code          */
+
+/*
+ * OSScTask flags:
+ */
+#define OS_SC_NEEDS_RDP 0x0001     /* uses the RDP */
+#define OS_SC_NEEDS_RSP 0x0002     /* uses the RSP */
+#define OS_SC_DRAM_DLIST 0x0004    /* SP & DP communicate through DRAM */
+#define OS_SC_PARALLEL_TASK 0x0010 /* must be first gfx task on list */
+#define OS_SC_LAST_TASK 0x0020     /* last task in queue for frame */
+#define OS_SC_SWAPBUFFER 0x0040    /* swapbuffers when gfx task done */
+
+#define OS_SC_RCP_MASK 0x0003  /* mask for needs bits */
+#define OS_SC_TYPE_MASK 0x0007 /* complete type mask */
+
 // TODO: This struct is in some way off. This should be fixed when we get to sched.c
 typedef struct {
     OSMesgQueue interruptQ;
-    OSMesg      intBuf[OS_SC_MAX_MESGS];
-    OSScMsg     retraceMsg;
-
+    OSMesg intBuf[OS_SC_MAX_MESGS];
     OSMesgQueue cmdQ;
-    OSMesg      cmdMsgBuf[OS_SC_MAX_MESGS];
-    OSScMsg     prenmiMsg;
-
-    OSThread    thread;
-    OSScClient  *clientList;
-    OSScTask    *audioListHead;
-    OSScTask    *gfxListHead;
-    OSScTask    *audioListTail;
-    OSScTask    *gfxListTail;
-    OSScTask    *curRSPTask;
-    OSScTask    *curRDPTask;
-    u32         frameCount;
-    s32         unk803156C0;
-    u32         unk803156C4; // safe type: u32
-    OSTime      unk803156C8;
-    OSTime      unk803156D0;
+    OSMesg cmdMsgBuf[OS_SC_MAX_MESGS];
+    OSThread thread;
+    OSScClient* clientList;
+    OSScTask* audioListHead;
+    OSScTask* gfxListHead;
+    OSScTask* audioListTail;
+    OSScTask* gfxListTail;
+    OSScTask* curRSPTask;
+    OSScTask* curRDPTask;
+    u32 frameCount;
+    s32 unk803156C0;
+    u32 retraceCount; // safe type: u32
+    OSTime unk803156C8;
+    OSTime unk803156D0;
 } OSSched;
 
-void            osCreateScheduler(OSSched *s, void *stack, OSPri priority,
-                                  u8 mode, u8 numFields);
-void            osScAddClient(OSSched *s, OSScClient *c, OSMesgQueue *msgQ, s32 arg3, s32 arg4);
-void            osScRemoveClient(OSSched *s, OSScClient *c);
-OSMesgQueue     *osScGetCmdQ(OSSched *s);
+void osCreateScheduler(OSSched* s, void* stack, OSPri priority, u8 mode, u8 numFields);
+void osScAddClient(OSSched* s, OSScClient* c, OSMesgQueue* msgQ, s32 arg3, s32 arg4);
+void osScRemoveClient(OSSched* s, OSScClient* c);
+OSMesgQueue* osScGetCmdQ(OSSched* s);
 
 #endif
-
